2d311eea1ef9c5c90a842bcde952803176ee8e5b
Eloy Bernardez <eloy.bernardez@zerobug.com.ar>
2024-02-11 16:16:32 -0300

    Se agregan otros tipos de Assert

--- StringManipulation.Tests/StringOperations.Test.cs
+++ StringManipulation.Tests/StringOperations.Test.cs
@@ -47,5 +47,61 @@
             // Assert
             Assert.False(result);
         }
+
+        [Fact]
+        public void RemoveWhiteSpace()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            string result = strOperations.RemoveWhitespace("Hello Platzi");
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.NotNull(result);
+            Assert.Equal("HelloPlatzi", result);
+        }
+
+        [Fact]
+        public void QuantityInWords()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            string result = strOperations.QuantintyInWords("perro", 12);
+
+            // Assert
+            Assert.NotEmpty(result);
+            Assert.NotNull(result);
+
+            Assert.StartsWith("doce", result);
+            Assert.Contains("perros", result);
+            //Assert.Equal("doce perros", result);
+        }
+
+        [Fact]
+        public void GetStringLength_Exception()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Assert
+            Assert.ThrowsAny<ArgumentNullException>(() => strOperations.GetStringLength(null));
+        }
+
+        [Fact]
+        public void GetStringLength()
+        {
+            // Arrange
+            var strOperations = new StringOperations();
+
+            // Act
+            int result = strOperations.GetStringLength("Hello Platzi");
+
+            // Assert
+            Assert.Equal(12, result);
+        }
     }
 }
